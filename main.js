1ae56d8d98cabc827c0d0f144747b0436fa1da29
Carolina Manhães <105887881+CarolinaMCF@users.noreply.github.com>
2023-09-12 08:16:07 -0300

    .

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ const ListaDeSons = document.querySelectorAll ('.tecla');
 
 let Contador = 0;
 
-while () {
+while (Contador < 9) {
     ListaDeSons[0].onclick = TocaSomPom;
+    Contador = Contador + 1;
 }
 
 
