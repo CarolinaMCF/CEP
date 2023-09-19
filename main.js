1bcf84c952e1a9494f7f1a5888b57373da442d0b
Carolina Manhães <105887881+CarolinaMCF@users.noreply.github.com>
2023-09-19 08:12:43 -0300

    .

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ const ListaDeSons = document.querySelectorAll ('.tecla');
 
 let Contador = 0;
 
+const classe = ListaDeSons[5].classList[1];
+console.log(classe)
+
+
 while (Contador < ListaDeSons.length) {
     ListaDeSons[Contador].onclick = function() {TocaSomPom('#som_tecla_pom');}
     Contador = Contador + 1;
