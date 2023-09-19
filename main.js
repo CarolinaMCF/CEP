84638aca4216881b4c46ab68b81f00524dda1f78
Carolina Manhães <105887881+CarolinaMCF@users.noreply.github.com>
2023-09-19 08:24:48 -0300

    5

--- main.js
+++ main.js
@@ -9,9 +9,14 @@ let Contador = 0;
 while (Contador < ListaDeSons.length) {
     ListaDeSons[Contador].onclick = function() {
     TocaSomPom('#som_tecla_pom');}
-    Contador = Contador + 1;
+
     const classe = ListaDeSons[Contador].classList[1];
+    
     console.log(classe);
+    
+    Contador = Contador + 1;
+
+
     console.log(Contador);
 }
 
