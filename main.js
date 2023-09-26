8c971c641bc043969ff85d695eb6004669ef8b67
Carolina Manhães <105887881+CarolinaMCF@users.noreply.github.com>
2023-09-26 08:04:00 -0300

    .

--- main.js
+++ main.js
@@ -7,8 +7,9 @@ const ListaDeSons = document.querySelectorAll ('.tecla');
 let Contador = 0;
 
 while (Contador < ListaDeSons.length) {
+    const tecla = lista[contador]
     const classe = ListaDeSons[Contador].classList[1];
-   console.log(classe);
+    console.log(classe);
 
     const idSom = `#som_${classe}`;
     console.log(idSom);
@@ -20,6 +21,9 @@ while (Contador < ListaDeSons.length) {
     Contador = Contador + 1;
 
     console.log(Contador);
+
+    tecla.classList.add('ativa');
+    tecla.classList.remove('ativa');
 }
 
 
