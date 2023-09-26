0b48c8afe796e83ecdd9dcfecee2a5afaa69842e
Carolina Manhães <105887881+CarolinaMCF@users.noreply.github.com>
2023-09-26 07:50:38 -0300

    .

--- main.js
+++ main.js
@@ -7,16 +7,19 @@ const ListaDeSons = document.querySelectorAll ('.tecla');
 let Contador = 0;
 
 while (Contador < ListaDeSons.length) {
-    ListaDeSons[Contador].onclick = function() {
-    TocaSomPom('#som_tecla_pom');}
-
     const classe = ListaDeSons[Contador].classList[1];
-    
-    console.log(classe);
+//console.log(classe);
+
+    const idSom = `#som_${classe}`;
+//console.log(idSom);
+
+    ListaDeSons[Contador].onclick = function() {
+    tocar(idSom);
+}
 
     Contador = Contador + 1;
 
-    console.log(Contador);
+//console.log(Contador);
 }
 
 
