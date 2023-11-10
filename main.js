8789d79eff0c7330b428a220d88dac04890bd701
Carolina Manhães <105887881+CarolinaMCF@users.noreply.github.com>
2023-11-10 17:57:11 -0300

    the blond o sonic shimmer trap

--- main.js
+++ main.js
@@ -35,14 +35,31 @@ const currentOperationText = document.querySelector('#current-operation');
 const buttons = document.querySelectorAll('#buttons-container button');
 
 class Calculator{
+  constructor(previousOperationText, currentOperationText){
+    this.previousOperationText = previousOperationText;
+    this.currentOperationText = currentOperationText;
+    this.currentOperation = "";
+  }
+
+  // Add digit to calculator screen
+  addDigit(digit){
+    this.currentOperation = digit;
+    this.updateScreen();
+  }
+  // Change values of the calculator screen
+  updateScreen(){
+    this.currentOperationText.innerText += this.currentOperation;
+  }
 }
+
+const calc = new Calculator(previousOperationText, currentOperationText);
 
 buttons.forEach((btn) => {
   btn.addEventListener("click",(e) => {
     const value = e.target.innerText;
 
     if(+value >= 0 || value === "."){
-        console.log(value);
+        calc.addDigit(value);
     }else{
        console.log("Op: " + value);
     }
