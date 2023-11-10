17f473de9e6a7c31d638bd52dd69c4a2ed704611
Carolina Manhães <105887881+CarolinaMCF@users.noreply.github.com>
2023-11-10 17:43:05 -0300

    value

--- main.js
+++ main.js
@@ -34,4 +34,14 @@ const previousOperationText = document.querySelector('#previous-operation');
 const currentOperationText = document.querySelector('#current-operation');
 const buttons = document.querySelectorAll('#buttons-container button');
 
-console.log(buttons);
+class Calculator{
+
+}
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click",(e) => {
+    const value = e.target.innerText;
+
+    console.log(value);
+  });
+});
