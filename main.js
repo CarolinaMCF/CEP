b603afc164576b1b6c39708c6d2e7147f36e1d0f
Carolina Manhães <105887881+CarolinaMCF@users.noreply.github.com>
2023-11-10 17:47:05 -0300

    do i wanna know

--- main.js
+++ main.js
@@ -35,13 +35,16 @@ const currentOperationText = document.querySelector('#current-operation');
 const buttons = document.querySelectorAll('#buttons-container button');
 
 class Calculator{
-
 }
 
 buttons.forEach((btn) => {
   btn.addEventListener("click",(e) => {
     const value = e.target.innerText;
 
-    console.log(value);
+    if(+value >= 0 || value === "."){
+        console.log(value);
+    }else{
+       console.log("Op: " + value);
+    }
   });
 });
